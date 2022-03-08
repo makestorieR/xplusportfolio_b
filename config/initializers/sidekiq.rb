0bfdcf29a07846ed98108f5e23a432eb16c2c1ce
john <johnnonso090@gmail.com>
2022-03-08 16:56:42 +0100

    configured sidekiq pool size

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -25,9 +25,9 @@
 
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDISCLOUD_URL'), size: 1, network_timeout: 5 }
+  config.redis = { url: ENV.fetch('REDISCLOUD_URL')}
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDISCLOUD_URL'), size: 12, network_timeout: 5 }
+  config.redis = { url: ENV.fetch('REDISCLOUD_URL') }
 end
