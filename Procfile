a1e5a16bcff13457e686e97b0149710db8ec125c
john <johnnonso090@gmail.com>
2022-03-08 16:35:41 +0100

    added a redis service

--- Procfile
+++ Procfile
@@ -1,4 +1,2 @@
-
-
-web: bundle exec rails server -p $PORT -e $RACK_ENV
-worker: bundle exec sidekiq -e $RACK_ENV -C config/sidekiq.yml
+web:    bundle exec puma -C config/puma.rb
+worker: bundle exec sidekiq -e production -C config/sidekiq.yml
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,26 +1,33 @@
 
 
- if Rails.env == "production" ? 
+#  if Rails.env == "production" ? 
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV.fetch('REDISTOGO_URL') }
-  end
+#   Sidekiq.configure_client do |config|
+#     config.redis = { url: ENV.fetch('REDISTOGO_URL') }
+#   end
   
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV.fetch('REDISTOGO_URL') }
-  end
-else 
+#   Sidekiq.configure_server do |config|
+#     config.redis = { url: ENV.fetch('REDISTOGO_URL') }
+#   end
+# else 
 
-    Sidekiq.configure_client do |config|
-        config.redis = { url: ENV['REDIS_PROVIDER'] }
-      end
+#     Sidekiq.configure_client do |config|
+#         config.redis = { url: ENV['REDIS_PROVIDER'] }
+#       end
     
-    Sidekiq.configure_server do |config|
-      config.redis = { url: ENV['REDIS_PROVIDER'] }
-    end
+#     Sidekiq.configure_server do |config|
+#       config.redis = { url: ENV['REDIS_PROVIDER'] }
+#     end
 
 
-end
+# end
   
 
 
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV.fetch('REDISCLOUD_URL'), size: 1, network_timeout: 5 }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV.fetch('REDISCLOUD_URL'), size: 12, network_timeout: 5 }
+end
