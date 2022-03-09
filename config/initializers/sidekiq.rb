9bc6b3f7d2d33a64bb1a145182303b25ed0a9d37
john <johnnonso090@gmail.com>
2022-03-09 14:36:32 +0100

    fixed the sidekiq worker

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,38 +1,5 @@
 
 
-#  if Rails.env.production?
-
- 
-#     Sidekiq.configure_client do |config|
-#       config.redis = { url: ENV.fetch('REDISCLOUD_URL')}
-#     end
-
-#     Sidekiq.configure_server do |config|
-#       config.redis = { url: ENV.fetch('REDISCLOUD_URL') }
-#     end
-# else 
-
-#     Sidekiq.configure_client do |config|
-#         config.redis = { url: ENV['REDIS_PROVIDER'] }
-#       end
-    
-#     Sidekiq.configure_server do |config|
-#       config.redis = { url: ENV['REDIS_PROVIDER'] }
-#     end
-
-
-# end
-  
-
-
-    # Sidekiq.configure_client do |config|
-    #     config.redis = { url: ENV['REDISTOGO_URL'] }
-    #   end
-    
-    # Sidekiq.configure_server do |config|
-    #   config.redis = { url: ENV['REDISTOGO_URL'] }
-    # end
-
     require 'sidekiq'
     Sidekiq.configure_client do |config|
         config.redis = { url: ENV['REDISTOGO_URL'] }
