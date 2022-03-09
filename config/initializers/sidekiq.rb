89c16efbe444cf1c2ebdc9e84f1b0d7ac2a0e6fe
john <johnnonso090@gmail.com>
2022-03-09 14:01:37 +0100

    fixed initializer file for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -25,10 +25,20 @@
   
 
 
-    Sidekiq.configure_client do |config|
-        config.redis = { url: ENV['REDISTOGO_URL'] }
-      end
+    # Sidekiq.configure_client do |config|
+    #     config.redis = { url: ENV['REDISTOGO_URL'] }
+    #   end
     
-    Sidekiq.configure_server do |config|
-      config.redis = { url: ENV['REDISTOGO_URL'] }
-    end
+    # Sidekiq.configure_server do |config|
+    #   config.redis = { url: ENV['REDISTOGO_URL'] }
+    # end
+
+    require 'sidekiq'
+
+Sidekiq.configure_client do |config|
+config.redis = { :size => 1 }
+end
+
+Sidekiq.configure_server do |config|
+config.redis = { :size => 4 }
+end
