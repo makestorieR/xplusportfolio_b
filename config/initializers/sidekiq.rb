972816619b63632620c6f4f29eb380fafed67a1c
john <johnnonso090@gmail.com>
2022-03-09 13:54:42 +0100

    fixed initializer file for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,16 +1,29 @@
 
 
- if Rails.env.production?
+#  if Rails.env.production?
 
  
-    Sidekiq.configure_client do |config|
-      config.redis = { url: ENV.fetch('REDISCLOUD_URL')}
-    end
+#     Sidekiq.configure_client do |config|
+#       config.redis = { url: ENV.fetch('REDISCLOUD_URL')}
+#     end
 
-    Sidekiq.configure_server do |config|
-      config.redis = { url: ENV.fetch('REDISCLOUD_URL') }
-    end
-else 
+#     Sidekiq.configure_server do |config|
+#       config.redis = { url: ENV.fetch('REDISCLOUD_URL') }
+#     end
+# else 
+
+#     Sidekiq.configure_client do |config|
+#         config.redis = { url: ENV['REDIS_PROVIDER'] }
+#       end
+    
+#     Sidekiq.configure_server do |config|
+#       config.redis = { url: ENV['REDIS_PROVIDER'] }
+#     end
+
+
+# end
+  
+
 
     Sidekiq.configure_client do |config|
         config.redis = { url: ENV['REDIS_PROVIDER'] }
@@ -19,8 +32,3 @@ else
     Sidekiq.configure_server do |config|
       config.redis = { url: ENV['REDIS_PROVIDER'] }
     end
-
-
-end
-  
-
