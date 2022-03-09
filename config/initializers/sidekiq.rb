a4068e02feb5e81367d92353bfe623b9ddf9c31a
john <johnnonso090@gmail.com>
2022-03-09 13:26:32 +0100

    fixed initializer file for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,11 +4,11 @@
 
  
     Sidekiq.configure_client do |config|
-      config.redis = { url: ENV.fetch('REDIS_URL')}
+      config.redis = { url: ENV.fetch('REDISCLOUD_URL')}
     end
 
     Sidekiq.configure_server do |config|
-      config.redis = { url: ENV.fetch('REDIS_URL') }
+      config.redis = { url: ENV.fetch('REDISCLOUD_URL') }
     end
 else 
 
@@ -24,15 +24,3 @@ else
 end
   
 
-# SIDEKIQ_REDIS_CONFIGURATION = {
-#   url: ENV[ENV["REDIS_PROVIDER"]], # if one assumes that REDIS_PROVIDER indirection is reliably present
-#   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }, # we must trust Heroku and AWS here
-# }
-
-# Sidekiq.configure_server do |config|
-#   config.redis = SIDEKIQ_REDIS_CONFIGURATION
-# end
-
-# Sidekiq.configure_client do |config|
-#   config.redis = SIDEKIQ_REDIS_CONFIGURATION
-# end
--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,14 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
 
 
-  if Rails.env.production? 
-    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
-        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
-    end
+  # if Rails.env.production? 
+  #   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+  #     ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+  #       ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  #   end
 
 
-  end
+  # end
 
   
   namespace :api do 
