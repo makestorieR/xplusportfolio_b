46da1793e9719ed354d5f59be13ed80bba8d3359
john <johnnonso090@gmail.com>
2022-03-09 16:36:47 +0100

    fixed the sidekiq worker

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,43 +1,42 @@
 
 
- if Rails.env.production?
+#  if Rails.env.production?
 
  
-    Sidekiq.configure_client do |config|
-      config.redis = { url: ENV['REDISTOGO_URL'] }
-    end
+#     Sidekiq.configure_client do |config|
+#       config.redis = { url: ENV['REDISTOGO_URL'] }
+#     end
 
-    Sidekiq.configure_server do |config|
-      config.redis = { url: ENV['REDISTOGO_URL'] }
-    end
-else 
+#     Sidekiq.configure_server do |config|
+#       config.redis = { url: ENV['REDISTOGO_URL'] }
+#     end
+# else 
 
-    Sidekiq.configure_client do |config|
-        config.redis = { url: ENV['REDIS_PROVIDER'] }
-      end
+#     Sidekiq.configure_client do |config|
+#         config.redis = { url: ENV['REDIS_PROVIDER'] }
+#       end
     
-    Sidekiq.configure_server do |config|
-      config.redis = { url: ENV['REDIS_PROVIDER'] }
-    end
+#     Sidekiq.configure_server do |config|
+#       config.redis = { url: ENV['REDIS_PROVIDER'] }
+#     end
 
 
-end
+# end
   
 
 
-    # Sidekiq.configure_client do |config|
-    #     config.redis = { url: ENV['REDISTOGO_URL'] }
-    #   end
+    Sidekiq.configure_client do |config|
+        config.redis = { url: ENV['REDISTOGO_URL'] }
+      end
     
-    # Sidekiq.configure_server do |config|
-    #   config.redis = { url: ENV['REDISTOGO_URL'] }
-    # end
+    Sidekiq.configure_server do |config|
+      config.redis = { url: ENV['REDISTOGO_URL'] }
+    end
 
-    # require 'sidekiq'
-    # Sidekiq.configure_client do |config|
-    #     config.redis = { url: ENV['REDISTOGO_URL'] }
-    #   end
+    Sidekiq.configure_client do |config|
+        config.redis = { url: ENV['REDISTOGO_URL'] }
+      end
     
-    # Sidekiq.configure_server do |config|
-    #   config.redis = { url: ENV['REDISTOGO_URL'] }
-    # end
+    Sidekiq.configure_server do |config|
+      config.redis = { url: ENV['REDISTOGO_URL'] }
+    end
