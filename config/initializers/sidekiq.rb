263922ae7575ed85aded7ac8627de7bec437014c
john <johnnonso090@gmail.com>
2022-03-09 15:44:14 +0100

    fixed the sidekiq worker

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,11 +4,11 @@
 
  
     Sidekiq.configure_client do |config|
-      config.redis = { url: ENV.fetch('REDIS_URL')}
+      config.redis = { url: ENV.fetch('REDIS_TLS_URL')}
     end
 
     Sidekiq.configure_server do |config|
-      config.redis = { url: ENV.fetch('REDIS_URL') }
+      config.redis = { url: ENV.fetch('REDIS_TLS_URL') }
     end
 else 
 
