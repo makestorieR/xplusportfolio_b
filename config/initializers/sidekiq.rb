33f6008a71841a968e930d1d65f9fa737807ede1
john <johnnonso090@gmail.com>
2022-03-09 16:40:04 +0100

    fixed the sidekiq worker

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,42 +1,44 @@
 
 
-#  if Rails.env.production?
+ if Rails.env.production?
 
  
-#     Sidekiq.configure_client do |config|
-#       config.redis = { url: ENV['REDISTOGO_URL'] }
-#     end
+    Sidekiq.configure_client do |config|
+      config.redis = { url: ENV.fetch('REDISTOGO_URL')}
+      config.redis = { url: ENV['REDISTOGO_URL'] }
+    end
 
-#     Sidekiq.configure_server do |config|
-#       config.redis = { url: ENV['REDISTOGO_URL'] }
-#     end
-# else 
+    Sidekiq.configure_server do |config|
+      config.redis = { url: ENV.fetch('REDISTOGO_URL') }
+    end
+else 
 
-#     Sidekiq.configure_client do |config|
-#         config.redis = { url: ENV['REDIS_PROVIDER'] }
-#       end
+    Sidekiq.configure_client do |config|
+        config.redis = { url: ENV['REDIS_PROVIDER'] }
+      end
     
-#     Sidekiq.configure_server do |config|
-#       config.redis = { url: ENV['REDIS_PROVIDER'] }
-#     end
+    Sidekiq.configure_server do |config|
+      config.redis = { url: ENV['REDIS_PROVIDER'] }
+    end
 
 
-# end
+end
   
 
 
-    Sidekiq.configure_client do |config|
-        config.redis = { url: ENV['REDISTOGO_URL'] }
-      end
+    # Sidekiq.configure_client do |config|
+    #     config.redis = { url: ENV['REDISTOGO_URL'] }
+    #   end
     
-    Sidekiq.configure_server do |config|
-      config.redis = { url: ENV['REDISTOGO_URL'] }
-    end
+    # Sidekiq.configure_server do |config|
+    #   config.redis = { url: ENV['REDISTOGO_URL'] }
+    # end
 
-    Sidekiq.configure_client do |config|
-        config.redis = { url: ENV['REDISTOGO_URL'] }
-      end
+    # require 'sidekiq'
+    # Sidekiq.configure_client do |config|
+    #     config.redis = { url: ENV['REDISTOGO_URL'] }
+    #   end
     
-    Sidekiq.configure_server do |config|
-      config.redis = { url: ENV['REDISTOGO_URL'] }
-    end
+    # Sidekiq.configure_server do |config|
+    #   config.redis = { url: ENV['REDISTOGO_URL'] }
+    # end
