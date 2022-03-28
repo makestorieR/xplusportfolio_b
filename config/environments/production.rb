9a6f5dc67bf0e242726d72dc10a92937b0b02e71
john <johnnonso090@gmail.com>
2022-03-28 13:53:37 +0100

    configured the mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "iamworkingon_b_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
 
   config.action_mailer.delivery_method = :smtp
