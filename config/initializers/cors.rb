1ecd851e6a818ed54abf3ddef990680cab2fb6ce
john <johnnonso090@gmail.com>
2022-04-02 12:35:19 +0100

    Fixed the cor client

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     if Rails.env.production?
             
-        origins ENV['CLIENT'] 
+        origins ENV['COR_CLIENT'] 
     else
         origins 'localhost:3000'
 
